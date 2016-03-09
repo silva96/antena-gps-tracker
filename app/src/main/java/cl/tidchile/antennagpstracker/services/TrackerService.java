444863e994d7747702f8388cb98001d8435fdddf
Benjamín Silva <silva96@gmail.com>
2016-03-09 16:00:24 -0300

    increasing gradle version and removing unused imports

--- app/src/main/java/cl/tidchile/antennagpstracker/services/TrackerService.java
+++ app/src/main/java/cl/tidchile/antennagpstracker/services/TrackerService.java
@@ -1,7 +1,6 @@
 package cl.tidchile.antennagpstracker.services;
 
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
